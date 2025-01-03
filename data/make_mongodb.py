a1509b227341d2a2cf8e3313f5f2e179f3eff667
yeowonh <hwyewon@gmail.com>
2025-01-03 21:39:19 +0900

    Add data & Modify prompt and configs

--- data/make_mongodb.py
+++ data/make_mongodb.py
@@ -19,26 +19,41 @@ CONFIG_FILE_NAME = "config.json"
 def main(config):
     parser = argparse.ArgumentParser()
     g = parser.add_argument_group("Settings")
-    g.add_argument("--jsonl_file_path", type=str, default=config["path"]["data_file_name"], help="data path")
+    g.add_argument("--file_path", type=str, default=config["path"]["data_file_name"], help="data path")
     g.add_argument("--db_name", type=str, default=config["path"]["db_name"], help="database name")
     g.add_argument("--collection_name", type=str, default=config["path"]["collection_name"], help="collection name")
     
     args = parser.parse_args()
 
     print("## Settings ##")
-    print("## jsonl_file_path : ", args.jsonl_file_path)
+    print("## file_path : ", args.file_path)
     print("## db_name : ", args.db_name)
-    print("## chunk_size : ", args.chunk_size)
-    print("## overlap_size : ", args.overlap_size)
+    print("## collection_name : ", args.collection_name)
 
     client = MongoClient(os.environ["MONGODB_ATLAS_CLUSTER_URI"])
     collection = client[args.db_name][args.collection_name]
     
-    with open(args.jsonl_file_path, 'r', encoding='utf-8') as f:
-        for document in f:
-            document_dict = ast.literal_eval(document)
-            result = collection.insert_one(document_dict)
-            print("## Document inserted with ID:", result.inserted_id)
+    # jsonl일 때
+    if args.file_path.split(".")[-1] == "jsonl":
+        with open(args.file_path, 'r', encoding='utf-8') as f:
+            print(f"## Upload {args.file_path} file : {len(f)} rows")
+            for document in f:
+                document_dict = ast.literal_eval(document)
+                result = collection.insert_one(document_dict)
+                print("## Document inserted with ID:", result.inserted_id)
+    
+    # json일 때
+    elif args.file_path.split(".")[-1] == "json":
+        with open(args.file_path, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            print(f"## Upload {args.file_path} file : {len(data)} rows")
+
+            for document in data:
+                result = collection.insert_one(document)
+                print("## Document inserted with ID:", result.inserted_id)
+
+    else:
+        raise ValueError("올바르지 않은 확장자 : json / jsonl 필요")
 
     print('## Document uploaded!')
     client.close()
--- data/utils.py
+++ data/utils.py
@@ -7,7 +7,88 @@ import re
 import ast
 embedding_columns = ["상담제목", "상담내용요약"]
 
+# return_type : embedding or generate
+# jsonl to metacsv
+"""
+경기도외국인상담센터 이외의 데이터 처리
+"""
 
+def get_external_result_csv(gpt_answer_path:str, meta_path: str, output_path: str, return_type: str, column_name:str) -> list:
+    """
+    gpt batch request를 받아, Document format에 맞추기
+    -> metadata와 매치하여 노드 전체 property + edge 관계를 담은 triplet list 반환
+    
+    output : jsonl 형태
+    """
+    
+    # jsonl result path
+    # UTF-8 인코딩으로 파일 열기
+    with open(gpt_answer_path, 'r', encoding='utf-8') as file:
+        data = []
+        for line in file:
+            # 각 줄을 JSON 객체로 파싱
+            json_obj = json.loads(line)
+            data.append(json_obj)
+
+
+    # 이전 meta csv 불러옴
+    if os.path.exists(meta_path):
+        result_csv = pd.read_csv(meta_path)
+        
+        result_csv['metadata'] = result_csv['metadata'].apply(ast.literal_eval)
+
+    else:
+        raise ValueError("No result meta csv")
+
+    ## generate 경우
+    if return_type == "generate":
+        for request in data:
+            answer = request["response"]["body"]["choices"][0]["message"]["content"]
+
+            info = re.sub(r"<\/?\w+>", "", answer)
+
+            # # 후처리
+            # info = [x for x in info.split('\n') if x != ""]
+            # solution = [x for x in solution.split('\n') if x != ""]
+
+            # 동일한 request_id 가진 metadata 불러와서 매칭하기
+
+            result_csv.loc[result_csv["request_id"] == request["custom_id"], "metadata"].values[0]["내담자_추가정보"] = info
+
+    
+    elif return_type == "embedding" and column_name in ["상담제목", "상담내용요약"]:
+        for request in data:
+            embedding = request['response']['body']['data'][0]['embedding']
+            result_csv.loc[result_csv["request_id"] == request["custom_id"], "metadata"].values[0][column_name]["Embedding"] = embedding
+    
+    else:
+        raise ValueError(f"## Wrong return type {return_type} or column name {column_name}!")
+    
+
+    # 덮어 씌우기
+    result_csv.to_csv(output_path, index=False)
+    print(f"## result saved in {output_path}")
+
+
+
+def get_external_result_jsonl(meta_path: str, result_jsonl_path: str):
+    # metadata csv
+    metadata_csv = pd.read_csv(meta_path)
+    metadata_csv['metadata'] = metadata_csv['metadata'].apply(ast.literal_eval)
+
+
+    with open(result_jsonl_path, 'w', encoding='utf-8') as f:
+        for _, row in metadata_csv.iterrows():
+            json_line = json.dumps(row['metadata'], ensure_ascii=False)
+            f.write(json_line + '\n')
+    
+    print(f"## jsonl file saved in {result_jsonl_path}")
+
+    return
+
+"""
+경기도외국인상담센터 데이터 처리
+"""
 # return_type : embedding or generate
 # jsonl to metacsv
 def get_result_csv(gpt_answer_path:str, meta_path: str, return_type: str, column_name:str) -> list:
@@ -92,7 +173,6 @@ def get_result_jsonl(meta_path: str, result_jsonl_path: str):
             f.write(json_line + '\n')
     
     print(f"## jsonl file saved in {result_jsonl_path}")
-
     return
 
 
